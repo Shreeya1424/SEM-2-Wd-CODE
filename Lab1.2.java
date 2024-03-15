e4f97417201b28f3a7307cfd928d0e79e62a7bda
Shreeya1424 <140989885+Shreeya1424@users.noreply.github.com>
2024-03-15 08:53:24 +0530

    Add files via upload

---
+++ Lab1.2.java
@@ -0,0 +1,60 @@
+public class Lab1.2{
+	 public static void main(String[] args) {
+		int a,b,c,sum;
+		a=100;
+		b=10;
+		c=100;
+		sum=a/b*c;
+		System.out.println(sum);
+	}
+}
+public class Lab11iii{
+	public static void main (String[]args){
+		int a,b,d,sum;
+		a=5;
+		b=4;
+		d=3;
+		sum=a*b/b*d;
+		System.out.println(sum);
+	}
+}
+public class Lab11iv{
+	public static void main(String[] args) {
+		int a,b,c,d,sum;
+		a=100;
+		b=200;
+		c=10;
+		d=3;
+		sum=a+b/c-d*c;
+		System.out.println(sum);
+	}
+}
+	
+public class lab12i{
+public static void main(String[] args) {
+	float a,b,sum,sub,mul,devi;
+	scanner sc = new scanner(System.in);
+	System.out.println("Enter a Number");
+	float a=sc.nextFloat;
+	float b=sc.nextFloat;
+	System.out.println("+,-,*,/,%":);
+	char c=sc.next();
+	switch(sc){
+		case +:
+		sum=a+b;
+		break;
+		switch(){
+		case -:
+		sub=a-b;
+		break;
+		switch(){
+		case *:
+		mul=a*b;
+		break;
+		switch(){
+		case /:
+		devi=a/b;
+		break;
+		System.out.println(sum);
+	}
+}
